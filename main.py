d8db88aff1946f130a526fb3a97a897cbcddb812
Sheldon Codling <codlinsh@gmail.com>
2023-01-10 22:10:55 -0500

    Added the code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
+# By Sheldon Codling
+# January 10th, 2023
 
 if __name__ == "__main__":
-    print("This is a test");
+    number = 0
+
+    while number < 1000000000:
+        number += 1
+        print(number)
+
+    print("I have made it to 1000000000!")
